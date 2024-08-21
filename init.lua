2a79c2e0d1f970df1d9efa72d259ec557fc737a1
v1rushb <cs.bashar.herbawi@gmail.com>
2024-08-21 13:35:21 +0300

    Update

--- init.lua
+++ init.lua
@@ -2,4 +2,8 @@ require('v1rushb');
 require('lsp');
 require('Comment').setup();
 vim.cmd("colorscheme cyberpunk");
+vim.cmd("set rnu");
+vim.cmd("set tabstop=4");
+vim.cmd("set shiftwidth=4");
+vim.cmd("set expandtab");
 print("Welcome to your vimmy world vimmer!");
